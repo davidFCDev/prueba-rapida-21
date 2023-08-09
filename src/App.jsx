48685cf30251b3236e7c552b1df7591cb03f6345
David Fernández <davidfernandezcomesana@gmail.com>
2023-08-09 23:31:08 +0200

    debounce movies

--- src/App.jsx
+++ src/App.jsx
@@ -1,74 +1,82 @@
-import { Movies } from "./components/Movies";
-import { useMovies } from "./hooks/useMovies";
-import { useEffect, useRef, useState } from "react";
-
-import "./App.css";
+import { Movies } from './components/Movies';
+import { useMovies } from './hooks/useMovies';
+import { useEffect, useRef, useState, useCallback } from 'react';
+import debounce from 'just-debounce-it';
+import './App.css';
 
 function useSearch() {
-  const [search, updateSearch] = useState("");
-  const [error, setError] = useState(null);
-  const isFirstInput = useRef(true);
+	const [search, updateSearch] = useState('');
+	const [error, setError] = useState(null);
+	const isFirstInput = useRef(true);
 
-  useEffect(() => {
-    if (isFirstInput.current) {
-      isFirstInput.current = search === "";
-      return;
-    }
-    if (search === "") {
-      setError("No se puede buscar una película vacía");
-      return;
-    }
+	useEffect(() => {
+		if (isFirstInput.current) {
+			isFirstInput.current = search === '';
+			return;
+		}
+		if (search === '') {
+			setError('No se puede buscar una película vacía');
+			return;
+		}
 
-    if (search.length < 3) {
-      setError("La búsqueda debe tener al menos 3 caracteres");
-      return;
-    }
+		if (search.length < 3) {
+			setError('La búsqueda debe tener al menos 3 caracteres');
+			return;
+		}
 
-    setError(null);
-  }, [search]);
+		setError(null);
+	}, [search]);
 
-  return { search, updateSearch, error };
+	return { search, updateSearch, error };
 }
 
 function App() {
-  const [sort, setSort] = useState(false);
-  const { search, updateSearch, error } = useSearch();
-  const { movies, getMovies, loading } = useMovies({ search, sort });
+	const [sort, setSort] = useState(false);
+	const { search, updateSearch, error } = useSearch();
+	const { movies, getMovies, loading } = useMovies({ search, sort });
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    getMovies({ search });
-  };
+	const debouncedGetMovies = useCallback(
+		debounce(search => {
+			getMovies({ search });
+		}, 300),
+		[getMovies]
+	);
 
-  const handleSort = () => {
-    setSort(!sort);
-  };
+	const handleSubmit = event => {
+		event.preventDefault();
+		getMovies({ search });
+	};
 
-  const handleChange = (event) => {
-    const newSearch = event.target.value;
-    updateSearch(newSearch);
-  };
+	const handleSort = () => {
+		setSort(!sort);
+	};
 
-  return (
-    <div className="page">
-      <header>
-        <h1>Buscador de películas</h1>
-        <form className="form" onSubmit={handleSubmit}>
-          <input
-            name="query"
-            onChange={handleChange}
-            value={search}
-            placeholder="Avengers, matrix..."
-          />
-          <input type="checkbox" onChange={handleSort} checked={sort} />
-          <button>Buscar</button>
-        </form>
-        {error && <p style={{ color: "red" }}>{error}</p>}
-      </header>
+	const handleChange = event => {
+		const newSearch = event.target.value;
+		updateSearch(newSearch);
+		debouncedGetMovies(newSearch);
+	};
 
-      <main>{loading ? <p>Loading...</p> : <Movies movies={movies} />}</main>
-    </div>
-  );
+	return (
+		<div className='page'>
+			<header>
+				<h1>Buscador de películas</h1>
+				<form className='form' onSubmit={handleSubmit}>
+					<input
+						name='query'
+						onChange={handleChange}
+						value={search}
+						placeholder='Avengers, matrix...'
+					/>
+					<input type='checkbox' onChange={handleSort} checked={sort} />
+					<button>Buscar</button>
+				</form>
+				{error && <p style={{ color: 'red' }}>{error}</p>}
+			</header>
+
+			<main>{loading ? <p>Loading...</p> : <Movies movies={movies} />}</main>
+		</div>
+	);
 }
 
 export default App;
